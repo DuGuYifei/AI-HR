7f1e8784123bfd222510c5666583d459aa60e0e4
Yifei Liu <yifei.liu@tum.de>
2025-05-30 06:42:36 +0200

    ci: Terraform and Ansible

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,23 +1,16 @@
 provider "aws" {
-  region = var.aws_region
+  region = "us-east-1"
 }
 
-resource "aws_key_pair" "deployer" {
-  key_name   = var.key_name
-  public_key = file(var.public_key_path)
-}
-
-resource "aws_instance" "app_server" {
-  ami                    = var.ami
-  instance_type          = var.instance_type
-  key_name               = aws_key_pair.deployer.key_name
+resource "aws_instance" "existing" {
+  ami                         = "ami-084568db4383264d4"
+  instance_type               = "t2.micro"
+  key_name                    = "vockey"
+  vpc_security_group_ids      = ["sg-01d6992f68e85ef75"]
+  subnet_id                   = "subnet-006efa2da636c282b"
   associate_public_ip_address = true
 
   tags = {
-    Name = "ai-hr-server"
-  }
-
-  provisioner "local-exec" {
-    command = "echo ${self.public_ip} > ../ansible/hosts.txt"
+    Name = "liu"
   }
 }
