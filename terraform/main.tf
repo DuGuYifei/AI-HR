e7a7574a477fa9aa890e6c775e79f22fca2ad13f
Yifei Liu <yifei.liu@tum.de>
2025-05-31 00:44:25 +0200

    ci: change RAM of AWS, successfully deployment

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "existing" {
   ami                         = "ami-084568db4383264d4"
-  instance_type               = "t2.micro"
+  instance_type               = "t3.small"
   key_name                    = "vockey"
   vpc_security_group_ids      = ["sg-01d6992f68e85ef75"]
   subnet_id                   = "subnet-006efa2da636c282b"
