f5845ffdf14866ac9bd455863c8c03d229f51d72
Yifei Liu <yifei.liu@tum.de>
2025-05-31 07:12:10 +0200

    ci: adjust size of database of k8s and test

--- helm/install.sh
+++ helm/install.sh
@@ -1,13 +1,13 @@
-helm upgrade --install aihr-dev ./helm/aihr \
-  --namespace aihr-dev --create-namespace \
+helm upgrade --install ai-hr-dev ./helm/aihr \
+  --namespace ai-hr-dev --create-namespace \
   -f helm/aihr/values-dev.yaml \
   --set global.ghcrUser=AET-DevOps25 \
   --set global.ghcrRepo=team-1 \
   --set global.imageTag=$(git rev-parse --short HEAD) \
   --set global.db.username=${DB_USER} # export DB_USER=postgres
 
-helm upgrade --install aihr-prod ./helm/aihr \
-  --namespace aihr --create-namespace \
+helm upgrade --install ai-hr-prod ./helm/aihr \
+  --namespace ai-hr --create-namespace \
   -f helm/aihr/values-prod.yaml \
   --set global.ghcrUser=aet-devops25 \
   --set global.ghcrRepo=team-1 \
